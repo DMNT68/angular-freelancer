a4e5fbbabff7c23528ddc615e41869288cacd4bd
Andrés Salgado <andressalgado41@gmail.com>
2020-02-12 18:40:45 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './components/header/header.module';
+import { AboutModule } from './components/about/about.module';
+import { ContactModule } from './components/contact/contact.module';
+import { CopyrightModule } from './components/copyright/copyright.module';
+import { FooterModule } from './components/footer/footer.module';
+import { NavbarModule } from './components/navbar/navbar.module';
+import { PortfolioModule } from './components/portfolio/portfolio.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    AboutModule,
+    ContactModule,
+    CopyrightModule,
+    FooterModule,
+    NavbarModule,
+    PortfolioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
